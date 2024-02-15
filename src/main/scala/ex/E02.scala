dd8374c4a09cdff0ed4bb82cca729beaff1e9cf2
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-15 16:59:47 +0400

    Add ex02.03 curry

--- src/main/scala/ex/E02.scala
+++ src/main/scala/ex/E02.scala
@@ -4,10 +4,15 @@ package ex
 import scala.annotation.tailrec
 
 object E02 {
+  // 02.02
   @tailrec
   def isSorted[A](as: Array[A], cmp: (A, A) => Boolean): Boolean =
     as match
       case Array() | Array(_) => true
       case _ =>
         cmp(as(0), as(1)) && isSorted(as.tail, cmp)
+
+  // 02.03
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
 }
--- src/test/scala/ex/E02Test.scala
+++ src/test/scala/ex/E02Test.scala
@@ -2,14 +2,21 @@ package ge.zgharbi.study.fps
 package ex
 
 import munit.FunSuite
+import org.junit.Assert
 import org.junit.Assert.*
 
 class E02Test extends FunSuite {
   import E02.*
-  test("isSorted") {
+  test("02.02 isSorted") {
     assertTrue(isSorted(Array(1, 2, 3), _ < _))
     assertTrue(isSorted(Array(3, 2, 1), _ > _))
     assertFalse(isSorted(Array(1, 2, 1), _ < _))
     assertFalse(isSorted(Array(1, 2, 3), _ > _))
+  }
+
+  test("02.03 curry") {
+    def mult(a: Int, b: Int): Int = a * b
+    def curriedMult = curry(mult)
+    assertEquals(mult(2, 3), curriedMult(2)(3))
   }
 }
