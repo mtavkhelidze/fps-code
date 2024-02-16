f4306692192c0431b0b4e2dfbe37c317c7512186
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-16 15:04:11 +0400

    Add ex 04.05 traverse, sequence

--- src/main/scala/ch/c04/Option.scala
+++ src/main/scala/ch/c04/Option.scala
@@ -27,4 +27,18 @@ enum Option[+A] {
 
 object Option {
   def apply[A](a: A): Option[A] = Some(a)
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  // using traverse
+  def sequence[A](as: List[Option[A]]): Option[List[A]] =
+    traverse(as)(identity)
+
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
+    as.foldRight(Some(Nil): Option[List[B]])((a, acc) =>
+      map2(f(a), acc)(_ :: _),
+    )
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(x => b.map(y => f(x, y)))
 }
--- src/test/scala/ex/E04Test.scala
+++ src/test/scala/ex/E04Test.scala
@@ -4,12 +4,28 @@ package ex
 import munit.FunSuite
 
 class E04Test extends FunSuite {
+  import ch.c04.Option
   import ch.c04.Option.*
-  import ex.E04.variance
 
   test("04.02 variance") {
+    import ex.E04.variance
+
     val v = variance(Seq(1.0, 2.0, 3.0, 4.0, 5.0))
     assertEquals(v, Some(2.0))
     assertEquals(variance(Nil), None)
+  }
+
+  test("04.03 Option#map2") {
+    val a = Some(10)
+    val b = Some(20)
+    assertEquals(map2(a, b)(_ + _), Some(30))
+  }
+
+  test("04.03 Option#map2") {
+    val as = List(Some(10), Some(20), Some(30))
+    assertEquals(sequence(as), Some(List(10, 20, 30)))
+
+    val bs = Some(11) :: None :: as
+    assertEquals(sequence(bs), None)
   }
 }
