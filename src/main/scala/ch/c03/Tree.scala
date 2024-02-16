c9950d603a8a6e3e30d8ae0f933f38bceff14dff
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-16 10:54:25 +0400

    Add 3.25 and 3.26

--- src/main/scala/ch/c03/Tree.scala
+++ src/main/scala/ch/c03/Tree.scala
@@ -1,9 +1,15 @@
 package ge.zgharbi.study.fps
 package ch.c03
 
+import scala.annotation.tailrec
+
 enum Tree[+A] {
   case Leaf(value: A)
   case Branch(left: Tree[A], right: Tree[A])
+
+  def map[B](f: A => B): Tree[B] = this match
+    case Leaf(value)         => Leaf(f(value))
+    case Branch(left, right) => Branch(left.map(f), right.map(f))
 }
 
 object Tree {
@@ -15,4 +21,12 @@ object Tree {
       t match
         case Leaf(value)         => value
         case Branch(left, right) => left.maximum max right.maximum
+
+    // 03.26
+    // Write a function, depth, that returns the maximum path length from the root of a tree to any leaf.
+    def depth[A](needle: Leaf[A]): Int =
+      t match
+        case Leaf(x) if x == needle.value => 1
+        case Branch(l, r) => 1 + l.depth(needle) max r.depth(needle)
+        case _            => 0
 }
--- src/test/scala/ex/E03Test.scala
+++ src/test/scala/ex/E03Test.scala
@@ -8,18 +8,26 @@ import munit.FunSuite
 class E03Test extends FunSuite {
   import Tree.*
 
-  test("03.25 Tree[Int]#maximum") {
-    import Tree.*
-    val tree: Tree[Int] = Branch(
+  val tree: Tree[Int] = Branch(
+    Branch(
+      Leaf(1),
       Branch(
-        Leaf(1),
-        Branch(
-          Leaf(2),
-          Leaf(11),
-        ),
+        Leaf(2),
+        Leaf(11),
       ),
-      Leaf(4),
-    )
+    ),
+    Leaf(4),
+  )
+
+  test("03.25 Tree[Int]#maximum") {
     assertEquals(11, tree.maximum)
+  }
+
+  test("03.26 Tree[Int]#depth") {
+    assertEquals(tree.depth(Leaf(11)), 2)
+  }
+
+  test("03.27 Tree[Int]#map") {
+      println(tree.map(x => s"value: ${x}"))
   }
 }
