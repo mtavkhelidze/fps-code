b5d075305cfa52155ea6b71ddf141b1fbedff8fa
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-20 00:03:18 +0400

    Add L06.05 Par base functions

--- src/main/scala/ch/c07/Par.scala
+++ src/main/scala/ch/c07/Par.scala
@@ -1,7 +1,33 @@
 package ge.zgharbi.study.fps
 package ch.c07
 
-type Par[A]
+import java.util.concurrent.{Callable, ExecutorService, TimeUnit, Future as JavaFuture}
+
+opaque type Par[A] = ExecutorService => JavaFuture[A]
+
+object Par {
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def unit[A](a: A): Par[A] = _ => UnitFuture(a)
+
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
+
+  private case class UnitFuture[A](get: A) extends JavaFuture[A] {
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+    override def get(timeout: Long, unit: TimeUnit): A = get
+    override def isCancelled: Boolean = false
+    override def isDone: Boolean = true
+  }
+
+  extension [A](pa: Par[A])
+    def run(s: ExecutorService): JavaFuture[A] = pa(s)
+
+    def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] =
+      (es: ExecutorService) =>
+        val fa = pa(es)
+        val fb = pb(es)
+        UnitFuture(f(fa.get, fb.get))
+}
 
 object Examples {
   import Par.*
@@ -10,15 +36,4 @@ object Examples {
     else
       val (l, r) = ints.splitAt(ints.size / 2)
       Par.fork(sum(l)).map2(Par.fork(sum(r)))(_ + _)
-}
-object Par {
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-
-  def unit[A](a: A): Par[A] = ???
-
-  def fork[A](a: => Par[A]): Par[A] = ???
-
-  extension [A](pa: Par[A])
-    def run: A = ???
-    def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] = ???
 }
