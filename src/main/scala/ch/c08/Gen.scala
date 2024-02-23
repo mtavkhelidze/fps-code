78238873ff3e332846b5d4b1864782220b0dffbd
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-23 11:03:22 +0400

    Add 08 update check with Either ret type

--- src/main/scala/ch/c08/Gen.scala
+++ src/main/scala/ch/c08/Gen.scala
@@ -1,11 +1,16 @@
 package ge.zgharbi.study.fps
 package ch.c08
+import ch.c08.Prop.SuccessCount
 
 import scala.annotation.targetName
 
 type Gen[+A]
+
+opaque type SuccessCount <: Int = Int
+opaque type FailedCase <: String = String
+
 trait Prop { self =>
-  def check: Boolean
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
   @targetName("and")
   def &&(that: Prop): Prop = new Prop:
     override def check: Boolean = self.check && that.check
