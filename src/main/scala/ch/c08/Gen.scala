51831e9e29ed25de4f56b6115a4bc1f66bb0677a
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-02-24 10:22:44 +0400

    Add E08.04 choose

--- src/main/scala/ch/c08/Gen.scala
+++ src/main/scala/ch/c08/Gen.scala
@@ -1,10 +1,11 @@
 package ge.zgharbi.study.fps
 package ch.c08
-import ch.c08.Prop.SuccessCount
+
+import ch.c06.{RNG, State}
 
 import scala.annotation.targetName
 
-type Gen[+A]
+opaque type Gen[+A] = State[RNG, A]
 
 opaque type SuccessCount <: Int = Int
 opaque type FailedCase <: String = String
@@ -13,9 +14,12 @@ trait Prop { self =>
   def check: Either[(FailedCase, SuccessCount), SuccessCount]
   @targetName("and")
   def &&(that: Prop): Prop = new Prop:
-    override def check: Boolean = self.check && that.check
+    override def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
 }
 
+def choose(start: Int, stopExclusive: Int): Gen[Int] =
+  State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start))
+  
 def listOf[A](gen: Gen[A]): Gen[List[A]] = listOfN(1, gen)
 
 def listOfN[A](n: Int, gen: Gen[A]): Gen[List[A]] = ???
