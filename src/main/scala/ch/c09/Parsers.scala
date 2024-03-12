ede5fc77e9469785d822f90efa2fc5ad5f4adaa1
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-03-12 10:28:42 +0400

    Add E09.13 string and regex

--- src/main/scala/ch/c09/Parsers.scala
+++ src/main/scala/ch/c09/Parsers.scala
@@ -158,7 +158,9 @@ case class ParseError(stack: List[(Location, String)]) {
 
 case class Location(input: String, offset: Int = 0) {
   def toError(msg: String): ParseError = ParseError(List((this, msg)))
-  
+
+  def advanceBy(n: Int): Location = copy(offset = offset + n)
+
   lazy val line: Int = input.slice(0, offset + 1).count(_ == '\n') + 1
   lazy val col: Int = input.slice(0, offset + 1).lastIndexOf('\n') match {
     case -1 => offset + 1
--- src/main/scala/ch/c09/ZedParser.scala
+++ src/main/scala/ch/c09/ZedParser.scala
@@ -1,6 +1,7 @@
 package ge.zgharbi.study.fps
 package ch.c09
 
+import ch.c09.Result.Failure
 import ch.ch09.{Location, ParseError, Parsers}
 
 import scala.util.matching.Regex
@@ -9,25 +10,50 @@ opaque type ZedParser[+A] = Location => Result[A]
 
 enum Result[+A] {
   case Success(get: A, consumed: Int)
-  case Failure(get: ParseError) extends Result[Nothing]
+  case Failure(get: ParseError, isCommitted: Boolean) extends Result[Nothing]
 }
 
 object ZedParser extends Parsers[ZedParser] {
-  override def string(s: String): ZedParser[String] = input =>
-    if s.startsWith(s) then Right(s)
-    else Left(Location(input).toError(s"Expected: $s"))
+  override def string(s: String): ZedParser[String] = loc =>
+    val i = firstNonMatchingIndex(loc.input, s, loc.offset)
+    if i == -1
+    then Result.Success(s, s.length)
+    else Result.Failure(loc.advanceBy(i).toError(s"Expected: $s"), i != 0)
 
-  override def regex(r: Regex): ZedParser[String] = ???
+  private def firstNonMatchingIndex(input: String, needle: String, offset: Int): Int = {
+    var i = 0
+    while i + offset < input.length && i < needle.length do
+      if input(i + offset) != needle(i) then return i
+      i += 1
+    if input.length - offset >= needle.length then -1
+    else input.length - offset
+  }
 
-  extension [A](kore: ZedParser[A])
+  override def regex(r: Regex): ZedParser[String] = loc =>
+    r.findPrefixOf(loc.input.substring(loc.offset)) match {
+      case Some(m) => Result.Success(m, m.length)
+      case None => Failure(loc.toError(s"Expected: $r"), false)
+    }
+
+  extension [A](kore: ZedParser[A]) {
+    override def succeed[T](a: T): ZedParser[T] =
+      _ => Result.Success(a, 0)
+
+    override def slice: ZedParser[String] =
+      loc => Result.Success(loc.input.take(loc.offset), loc.offset)
+
     override def attempt: ZedParser[A] = ???
+
     override def fail(msg: String): ZedParser[Nothing] = ???
+
     override def flatMap[B](f: A => ZedParser[B]): ZedParser[B] = ???
 
     override def label(l: String): ZedParser[A] = ???
+
     override def or(sore: => ZedParser[A]): ZedParser[A] = ???
+
     override def run(input: String): Either[ParseError, A] = ???
+
     override def scope(s: String): ZedParser[A] = ???
-    override def slice: ZedParser[String] = ???
-    override def succeed[T](a: T): ZedParser[T] = ???
+  }
 }
