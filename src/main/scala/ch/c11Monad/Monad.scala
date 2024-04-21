e322a36f647a5c9cd496a87a1c2f60a0f3a0c4dd
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-04-21 18:22:26 +0400

    Add Ch11 Monad trait

--- src/main/scala/ch/c11Monad/Monad.scala
+++ src/main/scala/ch/c11Monad/Monad.scala
@@ -1,6 +1,21 @@
 package ge.zgharbi.study.fps
 package ch.c11Monad
 
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A): F[A]
+
+  extension [A](fa: F[A]) {
+    def flatMap[B](f: A => F[B]): F[B]
+
+    override def map[B](f: A => B): F[B] = flatMap(a => unit(f(a)))
+
+    def map2[B, C](fb: F[B])(f: (A, B) => C): F[C] = {
+      fa.flatMap(a => fb.map(b => f(a, b)))
+    }
+  }
+
+}
+
 trait Functor[F[_]] {
   extension [A](fa: F[A]) {
     def map[B](f: A => B): F[B]
