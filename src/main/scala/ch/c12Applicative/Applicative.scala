c004ba1983f94acc3fe1f7d426a94d85a5dded70
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-05-07 14:50:50 +0400

    Bump some versions

--- src/main/scala/ch/c12Applicative/Applicative.scala
+++ src/main/scala/ch/c12Applicative/Applicative.scala
@@ -3,6 +3,27 @@ package ch.c12Applicative
 
 import ch.c11Monad.Functor
 
+object Applicative {
+  opaque type ZipList[+A] = LazyList[A]
+
+  object ZipList {
+    def apply[A](as: A*): ZipList[A] = LazyList.from(as)
+
+    extension [A](z: ZipList[A]) {
+      def toLazyList: LazyList[A] = z
+
+    }
+
+    given zipListApplicative: Applicative[ZipList] with {
+      override def unit[A](a: => A): ZipList[A] = LazyList(a)
+
+      extension [A](kore: ZipList[A])
+        override def map2[B, C](sore: ZipList[B])(f: (A, B) => C): ZipList[C] =
+          kore.zip(sore).map(f.tupled)
+    }
+  }
+}
+
 trait Applicative[F[_]] extends Functor[F] {
   def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
     fab.map2(fa)((fn, a) => fn(a))
